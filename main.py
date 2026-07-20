dc829faa0ccb12be5791cdd63368496f8c29f510
0000prinshu-ship-it <0000prinshu@gmail.com>
2026-07-20 20:07:05 +0545

    abstract class

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hi")
+from abc import ABC, abstractmethod
+
+class Inventory_Operations(ABC):
+
+    @abstractmethod
+    def view_medicines(self):
+        pass
+
+    @abstractmethod
+    def add_medicine(self):
+        pass
+
+    @abstractmethod
+    def remove_medicine(self):
+        pass
+#class Pharmacy_Inventory(Inventory_Operations):
